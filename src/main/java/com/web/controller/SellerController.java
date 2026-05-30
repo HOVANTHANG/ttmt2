81c811fce5e1919ef5d7116e469ecb5043ab26de
ThangKa <hothang2262004@gmail.com>
2026-05-30 10:55:59 +0700

    indon

--- src/main/java/com/web/controller/SellerController.java
+++ src/main/java/com/web/controller/SellerController.java
@@ -1,13 +1,23 @@
 package com.web.controller;
 
+import com.web.repository.InvoiceDetailRepository;
+import com.web.repository.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/seller")
 public class SellerController {
+
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+    @Autowired
+    private InvoiceDetailRepository invoiceDetailRepository;
     @RequestMapping(value = { "/index" }, method = RequestMethod.GET)
     public String dashboard() {
         return "seller/index";
@@ -66,6 +76,13 @@ public class SellerController {
     @RequestMapping(value = { "/diachi" }, method = RequestMethod.GET)
     public String diachi() {
         return "seller/diachi";
+    }
+
+    @RequestMapping(value = {"/in-don"}, method = RequestMethod.GET)
+    public String indon(Model model, @RequestParam Long id) {
+        model.addAttribute("hoaDon", invoiceRepository.findById(id).get());
+        model.addAttribute("ctHoaDon", invoiceDetailRepository.findByInvoiceId(id));
+        return "seller/indon";
     }
 
 }
